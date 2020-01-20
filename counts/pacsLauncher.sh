15bc980db1b291ce0d9ec127a3d64aff983331e2
2bPro <b.prodan95@gmail.com>
2020-01-20 15:32:01 +0000

    Add timestamp to logs and bak files

--- counts/pacsLauncher.sh
+++ counts/pacsLauncher.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd /beegfs-hdruk/smi/data/counts
 
-cp dicom_counts.py dicom_counts.bak
+timestamp=`date "+%Y-%m-%d_%H:%M:%S"`
+log="logs/pacscounter-${timestamp}.log"
+count="counts/dicomCounts-${timestamp}.json.bak"
 
 # '-u' to disable output buffering so progress is written to log file more often
-python -u pacscounter.py &> pacscounter.log
+python -u pacscounter.py &> $log
+
+cp dicom_counts.py $count
